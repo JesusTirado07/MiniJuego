6168f8e6d853292409d57fad590a66022af0fd87
JesusTirado07 <tirado.jart@gmail.com>
2024-05-15 22:48:44 -0600

    accionBotonJS

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,13 @@
+function iniciarJuego() {
+    
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+}
+
+function seleccionarMascotaJugador() {
+    alert("Seleccionaste tu mascota")
+}
+
+
+window.addEventListener("load", iniciarJuego)
