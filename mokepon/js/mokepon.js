efdc5b543d29ba445b7274624274434a11a7de4a
JesusTirado07 <tirado.jart@gmail.com>
2024-05-22 17:23:01 -0600

    SeleccionOpcionFunction

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,7 +6,38 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    alert("Seleccionaste tu mascota")
+    let inputHipoge = document.getElementById("hipoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya = document.getElementById("ratigueya")
+    let inputTenazas = document.getElementById("tenazas")
+    let inputTucapalma = document.getElementById("tucapalma")
+
+    if (inputHipoge.checked){
+
+        alert("Has escogido a Hipoge")
+
+    }  else if (inputCapipepo.checked){
+
+        alert("Has escogido a Capipepo")
+
+    } else if (inputRatigueya.checked){
+
+        alert("Has escogido a Ratigueya")
+
+    } else if (inputTenazas.checked){
+
+        alert("Has escogido a Tenazas")
+
+    } else if (inputTucapalma.checked){
+
+        alert("Has escogido a Tucapalma")
+        
+    } else {
+
+        alert("No has escogido ninguna mascota")
+        
+    }
+
 }
 
 
